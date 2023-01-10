e20588d590edbccb4b12caea348fdda628d307e6
Anjana19JK <anjanajoseph199@gmail.com>
2023-01-11 01:03:58 +0530

    Update index.js

--- src/components/EventItem/index.js
+++ src/components/EventItem/index.js
@@ -1,15 +1,21 @@
 const EventItem = props => {
-  const {eventDetails, onClickItem} = props
+  const {isActive, eventDetails, clickTabItem} = props
   const {id, name, imageUrl, location, registrationStatus} = eventDetails
+  const btnClassName = isActive
+    ? 'language-btn active-language-btn'
+    : 'language-btn'
 
-  onClickItem = () => {
-    clickTabItem(id)
+  const onClickItem = () => {
+    clickTabItem(id, registrationStatus)
   }
 
   return (
     <li>
-      <img src={imageUrl} alt={registrationStatus} onClick={onClickItem} />
-      <h1>{name}</h1>
+      <button type="button" className={btnClassName} onClick={onClickItem}>
+        <img src={imageUrl} alt="event" />
+      </button>
+
+      <p>{name}</p>
       <p>{location}</p>
     </li>
   )
